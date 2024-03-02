a84dd4d3eac2174ce0c2ae87d31856f0c0177eb9
Edwin Lima <edwin.limaa@gmail.com>
2024-03-02 15:29:15 -0300

    Controller using Correct HttpStatus, remove warning open-in-view

--- rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/controllers/PersonController.kt
+++ rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/controllers/PersonController.kt
@@ -3,11 +3,16 @@ package br.com.erudio.controllers
 import br.com.erudio.model.Person
 import br.com.erudio.services.PersonService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 
@@ -17,38 +22,39 @@ class PersonController {
 
     @Autowired
     private lateinit var service: PersonService
-    @RequestMapping(method = [RequestMethod.GET],
+    @GetMapping(
         produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE])
-    fun findAllPersons(): List<Person> {
-        return service.findAll()
+    fun findAllPersons(): ResponseEntity<List<Person>> {
+        return ResponseEntity.ok(service.findAll())
     }
 
-    @RequestMapping(value = ["/{id}"], method = [RequestMethod.GET],
+    @GetMapping(value = ["/{id}"],
         produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE])
-    fun findPerson(@PathVariable(value = "id") id:Long): Person {
+    fun findPerson(@PathVariable(value = "id") id:Long): ResponseEntity<Person> {
 
-        return service.findById(id)
+        return ResponseEntity.ok(service.findById(id))
     }
 
-    @RequestMapping(method = [RequestMethod.POST],
+    @PostMapping(
         consumes = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE])
-    fun createPerson(@RequestBody person: Person): Person {
+    fun createPerson(@RequestBody person: Person): ResponseEntity<Person> {
 
-        return service.create(person)
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(person))
     }
-    @RequestMapping(method = [RequestMethod.PUT],
+    @PutMapping(
         consumes = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE])
-    fun updatePerson(@RequestBody person: Person): Person {
+    fun updatePerson(@RequestBody person: Person): ResponseEntity<Person> {
 
-        return service.update(person)
+        return ResponseEntity.ok(service.update(person))
     }
 
-    @RequestMapping(value = ["/{id}"], method = [RequestMethod.DELETE],
+    @DeleteMapping(value = ["/{id}"],
         produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE])
-    fun deletePerson(@PathVariable(value = "id") id:Long) {
+    fun deletePerson(@PathVariable(value = "id") id:Long): ResponseEntity<*> {
 
         service.delete(id)
+        return ResponseEntity.noContent().build<Any>()
     }
 }
