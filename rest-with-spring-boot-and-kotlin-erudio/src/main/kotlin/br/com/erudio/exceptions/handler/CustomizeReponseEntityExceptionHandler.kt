26e2ec020db8f7db1290ff8d0b00866634482779
Edwin Lima <edwin.limaa@gmail.com>
2024-03-02 12:30:38 -0300

    Integrando ao banco de dados inicio.

--- rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/exceptions/handler/CustomizeReponseEntityExceptionHandler.kt
+++ rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/exceptions/handler/CustomizeReponseEntityExceptionHandler.kt
@@ -1,15 +1,16 @@
-package br.com.erudio.exceptions
+package br.com.erudio.exceptions.handler
 
+import br.com.erudio.exceptions.ExceptionResponse
+import br.com.erudio.exceptions.ResourceNotFoundException
 import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
-import java.lang.*
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
-import java.util.Date
+import java.lang.Exception
+import java.util.*
 
 @ControllerAdvice
 @RestController
@@ -17,8 +18,8 @@ class CustomizeReponseEntityExceptionHandler : ResponseEntityExceptionHandler() 
 
     @ExceptionHandler(Exception::class)
     fun handlerAllExceptions(ex: Exception, request: WebRequest) :
-            ResponseEntity<ExceptionResponse>{
-        val exceptionResponse =  ExceptionResponse(
+            ResponseEntity<ExceptionResponse> {
+        val exceptionResponse = ExceptionResponse(
             Date(),
             ex.message,
             request.getDescription(false)
@@ -27,15 +28,15 @@ class CustomizeReponseEntityExceptionHandler : ResponseEntityExceptionHandler() 
         return ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR)
     }
 
-    @ExceptionHandler(UnsuportedMathOperationException::class)
-    fun handlerBadRequestExceptions(ex: Exception, request: WebRequest) :
-            ResponseEntity<ExceptionResponse>{
-        val exceptionResponse =  ExceptionResponse(
+    @ExceptionHandler(ResourceNotFoundException::class)
+    fun handlerResourceNotFoundExceptions(ex: Exception, request: WebRequest) :
+            ResponseEntity<ExceptionResponse> {
+        val exceptionResponse = ExceptionResponse(
             Date(),
             ex.message,
             request.getDescription(false)
         )
 
-        return ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.BAD_REQUEST)
+        return ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.NOT_FOUND)
     }
 }
