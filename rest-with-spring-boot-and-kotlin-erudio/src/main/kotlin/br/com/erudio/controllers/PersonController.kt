4ee2ade3a776a60f02514e8f793f10317b354922
Edwin Lima <edwin.limaa@gmail.com>
2024-03-03 00:21:45 -0300

    Excemplo de versionamento de VO fins educacionais.

--- rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/controllers/PersonController.kt
+++ rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/controllers/PersonController.kt
@@ -1,6 +1,7 @@
 package br.com.erudio.controllers
 
 import br.com.erudio.data.vo.v1.PersonVO
+import br.com.erudio.data.vo.v2.PersonVO as PersonVOV2
 import br.com.erudio.services.PersonService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
@@ -41,6 +42,14 @@ class PersonController {
     fun createPerson(@RequestBody person: PersonVO): ResponseEntity<PersonVO> {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(service.create(person))
+    }
+
+    @PostMapping(value = ["/v2"],
+        consumes = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE])
+    fun createPersonV2(@RequestBody person: PersonVOV2): ResponseEntity<PersonVOV2> {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.createV2(person))
     }
     @PutMapping(
         consumes = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE],
