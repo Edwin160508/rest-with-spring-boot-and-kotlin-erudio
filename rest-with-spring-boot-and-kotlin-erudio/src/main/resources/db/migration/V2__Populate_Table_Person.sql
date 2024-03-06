b6caf0be061ed921e8c5d5cc7c73ccba8e0c8690
Edwin Lima <edwin.limaa@gmail.com>
2024-03-06 09:45:37 -0300

    Integrando Flyway, criando migrations.

---
+++ rest-with-spring-boot-and-kotlin-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Campina Grande - Paraíba - Brasil', 'Edwin', 'Male', 'Lima'),
+	(2, 'Campina Grande - Paraíba - Brasil', 'Pâmela Priscilla', 'Female', 'Silva'),
+	(4, 'Campina Grande - Paraíba - Brasil', 'Everton', 'Male', 'Lima'),
+	(5, 'Campina Grande - Paraíba - Brasil', 'Andrea', 'Female', 'Muniz'),
+	(6, 'Campina Grande - Paraíba - Brasil', 'Beta', 'Female', 'Lima'),
+	(7, 'Campina Grande - Paraíba - Brasil', 'Samuel', 'Male', 'Muniz');
