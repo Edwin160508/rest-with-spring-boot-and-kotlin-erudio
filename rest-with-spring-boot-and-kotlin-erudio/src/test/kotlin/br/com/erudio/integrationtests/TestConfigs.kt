230d9c47f9b64c4c947536f29cfc885aa0f5b8ce
Edwin Lima <edwin.limaa@gmail.com>
2024-03-21 11:41:51 -0300

    Criando VOs  Book Token e Account, TestConfig parametro Authorization.

--- rest-with-spring-boot-and-kotlin-erudio/src/test/kotlin/br/com/erudio/integrationtests/TestConfigs.kt
+++ rest-with-spring-boot-and-kotlin-erudio/src/test/kotlin/br/com/erudio/integrationtests/TestConfigs.kt
@@ -8,6 +8,7 @@ object TestConfigs {
     const val CONTENT_TYPE_YAML = "application/x-yaml"
 
     const val HEADER_PARAM_ORIGIN = "Origin"
+    const val HEADER_PARAM_AUTHORIZATION = "Authorization"
 
     const val ORIGIN_ERUDIO = "https://erudio.com.br"
     const val ORIGIN_SEMERU = "https://semeru.com.br"
